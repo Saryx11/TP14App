51dba63f0927b9a635df0b7735ad4be3583f81d2
Unknown <louis.benjamin@live.fr>
2017-10-17 15:01:17 +0200

    handler working

--- app/src/main/java/com/example/benjaminlouis/tp14app/MainActivity.java
+++ app/src/main/java/com/example/benjaminlouis/tp14app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.benjaminlouis.tp14app;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,23 +10,37 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler handlerTest;
+    ProgressBar barre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button go = (Button) findViewById(R.id.go);
+        barre = (ProgressBar) findViewById(R.id.progress);
+
+        handlerTest = new Handler(){
+        public void handleMessage(Message msg){
+            int progress=msg.getData().getInt("PROGRESS");
+            MainActivity.this.barre.incrementProgressBy(progress);
+        }  };
+
+
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Thread prog = new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        ProgressBar barre = (ProgressBar) findViewById(R.id.progress);
+                        Bundle messageBundle = new Bundle();
                         try {
-                            while (barre.getProgress() < barre.getMax()) {
-                                barre.incrementProgressBy(1);
+                            while (MainActivity.this.barre.getProgress() < MainActivity.this.barre.getMax()) {
                                 Thread.sleep(300);
+                                Message msg = handlerTest.obtainMessage();
+                                messageBundle.putInt("PROGRESS",1);
+                                msg.setData(messageBundle);
+                                handlerTest.sendMessage(msg);
                             }
                         } catch (InterruptedException e) {
                             e.printStackTrace();
